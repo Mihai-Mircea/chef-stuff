b68ae388ef89423897ea5869f7bbfe55fc1c53e4
Mihai-Mircea <mihai.github@gmail.com>
2017-07-22 17:16:30 +0000

    added motd and chnaged apache default for notde atribute

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,12 +4,19 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+if node['platform_family'] == "debian"
+	package = "apache2"
+elsif node['platform_family'] == "rhel"
+	package = "httpd"
+end
+
 package 'apache2' do
 	package_name 'apache2'
 	action :install
 end
 
 service 'apache2' do
+	supports :status => true
 	service_name 'apache2'
 	action [:start,:enable]
 end
